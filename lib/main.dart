88cce960d0496d703da83b7b0de073bbf7edc41e
Hanzvalz <Valmoria@gmail.com>
2021-01-10 21:56:43 +0800

    main check login status

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,8 +21,60 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  SharedPreferences sharedPreferences;
+  checkLoginStatus() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    if (sharedPreferences.getString("token") == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+          (Route<dynamic> route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nodejs-Mongodb", style: TextStyle(color: Colors.white)),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              sharedPreferences.clear();
+              sharedPreferences.commit();
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => LoginPage()),
+                  (Route<dynamic> route) => false);
+            },
+            child: Text("Log Out", style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
+      body: Center(child: Text("Main Page")),
+      drawer: Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('Ejercicios'),
+              accountEmail: new Text('codigoalphacol@gmail.com'),
+            ),
+            new ListTile(
+                title: new Text("List Products"),
+                trailing: new Icon(Icons.help),
+                onTap: () {}),
+            new ListTile(
+                title: new Text("Add Products"),
+                trailing: new Icon(Icons.help),
+                onTap: () {}),
+            new Divider(),
+            new ListTile(
+              title: new Text("Register user"),
+              trailing: new Icon(Icons.fitness_center),
+              onTap: () => {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
