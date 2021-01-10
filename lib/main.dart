06f92bdf5a5eb706ab218f5d3c5bd0199a51a9e7
Hanzvalz <Valmoria@gmail.com>
2021-01-10 20:43:17 +0800

    error in main dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Swift Book Seller",
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+      theme: ThemeData(accentColor: Colors.white70),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
